240b35f0e7983a196a7511d9b4ca3f9292f19034
JoseCordova11 <73542968+JoseCordova11@users.noreply.github.com>
2021-05-20 17:34:48 -0500

    Update Ejecutor.java

--- EjemploSerializacion01/EjemploArchivosSerializacion/src/paquete4/Ejecutor.java
+++ EjemploSerializacion01/EjemploArchivosSerializacion/src/paquete4/Ejecutor.java
@@ -5,5 +5,32 @@ package paquete4;
  * @author Jose Cordova
  */
 public class Ejecutor {
+    public static void main(String[] args) {
+
+        String nombreArchivo = "hospitales.txt";
+
+        Hospital h1 = new Hospital("Hospital UTPL", 30, 750.00);
+        Hospital h2 = new Hospital("San Agustín", 5000.99);
+        Hospital h3 = new Hospital("Isidro Ayora", 60, 990.50);
+
+ 
+        EscrituraArchivoSecuencial archivo = new EscrituraArchivoSecuencial
+        (nombreArchivo);
+        
+        archivo.establecerRegistro(h1);
+        archivo.establecerSalida();
+        archivo.establecerRegistro(h2);
+        archivo.establecerSalida();
+        archivo.establecerRegistro(h3);
+        archivo.establecerSalida();
+        archivo.cerrarArchivo();
+    
+        LecturaArchivoSecuencial lectura = new LecturaArchivoSecuencial
+        (nombreArchivo);
+        lectura.establecerListaHospital();
+        System.out.println(lectura);
+
+    }
+    
     
 }
